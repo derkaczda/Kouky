b3100135bc91ef6586590b8ca245d187a7a9feab
Daniel <daniel.c.derkacz@t-online.de>
2020-03-15 16:39:24 +0100

    change position of model instance

--- Models/ModelInstance/index.js
+++ Models/ModelInstance/index.js
@@ -13,6 +13,24 @@ export default class ModelInstance
         this.scale = scale;
     }
 
+    getX = () => this.x;
+    getY = () => this.y;
+    getZ = () => this.z;
+    getRX = () => this.rx;
+    getRY = () => this.ry;
+    getRZ = () => this.rz;
+    getScale = () => this.scale;
+
+    setX = (x) => { this.x = x; this.updateTransformationMatrix(); }
+    setY = (y) => { this.y = y; this.updateTransformationMatrix(); }
+    setZ = (z) => { this.z = z; this.updateTransformationMatrix(); }
+    setRX = (rx) => { this.rx = rx; this.updateTransformationMatrix(); }
+    setRY = (ry) => { this.ry = ry; this.updateTransformationMatrix(); }
+    setRZ = (rz) => { this.rz = rz; this.updateTransformationMatrix(); }
+    setScale = (scale) => { this.scale = scale; this.updateTransformationMatrix(); }
+
+    setPosition = (position) => { this.x = position.x(); this.y = position.y(); this.z = position.z(); this.updateTransformationMatrix(); }
+
     updateRotation = (rx, ry, rz) =>
     {
         this.rx += rx;
